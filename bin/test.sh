6055e99ebba9c27ae43cf9dc48a0fac8d8a40353
Gustavo Hoirisch <gugahoi@users.noreply.github.com>
2022-12-02 10:36:28 +1100

    Update test script to curl
    Curl is ubiquitous so less likely to have problems

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 while true; do
-	http GET https://api-cdn-cache.web.app/date
+	response=$(curl https://api-cdn-cache.web.app/date -v 2>&1)
+	hit_amount=$(echo "$response" | awk '/x-cache-hits/ { print $3 }')
+	status_code=$(echo "$response" | awk '/^< HTTP\/2/ { print $3 }')
+	cache_control=$(echo "$response" | grep 'cache-control')
+
+	echo "---------------------------------"
+	echo "Cache Hits: ${hit_amount}"
+	echo "Status: ${status_code}"
+	echo "Cache Control Header: ${cache_control}"
+
 	sleep 2
 done
