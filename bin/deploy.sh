a3da821d377de114516ad9de856ef565f9fceeb2
Gustavo Hoirisch <gugahoi@users.noreply.github.com>
2022-12-01 14:31:59 +1100

    Update docker build for amd64 platform

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-set +x
+set +euo pipefail
 
 IMAGE_URL=australia-southeast1-docker.pkg.dev/api-demo-94c7a/api-demo
 VERSION="$(git rev-parse head)"
-TAG="${IMAGE_URL}":"${VERSION}"
+TAG="${IMAGE_URL}/api":"${VERSION}"
 
+set -x
 echo "--- building image"
-docker build -f Dockerfile -t "${TAG}" .
-
-echo "--- pushing image"
-docker push "${TAG}"
+docker buildx build --platform linux/amd64 --push -t "${TAG}" .
+# docker build -f Dockerfile -t "${TAG}" .
 
 echo "--- deploying image"
 gcloud run deploy api --image "${TAG}" \
 	--platform managed \
 	--port 7979 \
+	--project api-demo-94c7a \
 	--region australia-southeast1
