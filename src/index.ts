74a3b2af61bd5baae1cfe976d9ee8141841420e2
Gustavo Hoirisch <gugahoi@users.noreply.github.com>
2022-12-01 14:44:26 +1100

    Add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 let counter = 0;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/date", (_, res) => {
